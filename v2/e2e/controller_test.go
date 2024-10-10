5f596d308c518ca240ae70144c118ad8741caad4
Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>
2024-10-10 11:40:06 +0200

    Updated tests according to the CR
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>

--- v2/e2e/controller_test.go
+++ v2/e2e/controller_test.go
@@ -3,6 +3,7 @@ package e2e
 import (
 	"bytes"
 	"fmt"
+	"os"
 
 	coilv2 "github.com/cybozu-go/coil/v2/api/v2"
 	. "github.com/onsi/ginkgo/v2"
@@ -11,68 +12,78 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+const (
+	testIPv6Key   = "TEST_IPV6"
+	testIPAMKey   = "TEST_IPAM"
+	testEgressKey = "TEST_EGRESS"
+)
+
 var _ = Describe("coil-ipam-controller", func() {
-	if testIPAM {
-		It("should elect a leader instance of coil-controller", func() {
-			kubectlSafe(nil, "-n", "kube-system", "get", "leases", "coil-ipam-leader")
-		})
+	if os.Getenv(testIPAMKey) == "true" {
+		Context("when the IPAM features are enabled", func() {
+			It("should elect a leader instance of coil-controller", func() {
+				kubectlSafe(nil, "-n", "kube-system", "get", "leases", "coil-ipam-leader")
+			})
 
-		It("should run the admission webhook", func() {
-			By("trying to create an invalid AddressPool")
-			_, err := kubectl(nil, "apply", "-f", "manifests/invalid_pool.yaml")
-			Expect(err).Should(HaveOccurred())
-		})
+			It("should run the admission webhook", func() {
+				By("trying to create an invalid AddressPool")
+				_, err := kubectl(nil, "apply", "-f", "manifests/invalid_pool.yaml")
+				Expect(err).Should(HaveOccurred())
+			})
 
-		It("should export metrics", func() {
-			pods := &corev1.PodList{}
-			getResourceSafe("kube-system", "pods", "", "app.kubernetes.io/component=coil-ipam-controller", pods)
-			Expect(pods.Items).Should(HaveLen(2))
+			It("should export metrics", func() {
+				pods := &corev1.PodList{}
+				getResourceSafe("kube-system", "pods", "", "app.kubernetes.io/component=coil-ipam-controller", pods)
+				Expect(pods.Items).Should(HaveLen(2))
 
-			node := pods.Items[0].Spec.NodeName
-			out, err := runOnNode(node, "curl", "-sf", "http://localhost:9386/metrics")
-			Expect(err).ShouldNot(HaveOccurred())
+				node := pods.Items[0].Spec.NodeName
+				out, err := runOnNode(node, "curl", "-sf", "http://localhost:9386/metrics")
+				Expect(err).ShouldNot(HaveOccurred())
 
-			mfs, err := (&expfmt.TextParser{}).TextToMetricFamilies(bytes.NewReader(out))
-			Expect(err).NotTo(HaveOccurred())
-			Expect(mfs).NotTo(BeEmpty())
-		})
+				mfs, err := (&expfmt.TextParser{}).TextToMetricFamilies(bytes.NewReader(out))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(mfs).NotTo(BeEmpty())
+			})
 
-		It("should run garbage collector", func() {
-			By("creating an orphaned AddressBlock")
-			kubectlSafe(nil, "apply", "-f", "manifests/orphaned.yaml")
+			It("should run garbage collector", func() {
+				By("creating an orphaned AddressBlock")
+				kubectlSafe(nil, "apply", "-f", "manifests/orphaned.yaml")
 
-			By("checking the AddressBlock gets removed")
-			Eventually(func() interface{} {
-				abl := &coilv2.AddressBlockList{}
-				err := getResource("", "addressblocks", "", "coil.cybozu.com/node=coil-worker100", abl)
-				if err != nil {
-					return err
-				}
-				return abl.Items
-			}, 20).Should(BeEmpty())
+				By("checking the AddressBlock gets removed")
+				Eventually(func() interface{} {
+					abl := &coilv2.AddressBlockList{}
+					err := getResource("", "addressblocks", "", "coil.cybozu.com/node=coil-worker100", abl)
+					if err != nil {
+						return err
+					}
+					return abl.Items
+				}, 20).Should(BeEmpty())
+			})
 		})
 	}
 })
 
-var _ = Describe("coil-egress-controller", func() {
-	if testEgress {
-		It("should elect a leader instance of coil-controller", func() {
-			kubectlSafe(nil, "-n", "kube-system", "get", "leases", "coil-egress-leader")
-		})
+var _ = Describe("coil-egress-controllerr", func() {
+	if os.Getenv(testEgressKey) == "true" {
+		Context("when the egress features are enabled", func() {
+			It("should elect a leader instance of coil-controller", func() {
+				kubectlSafe(nil, "-n", "kube-system", "get", "leases", "coil-egress-leader")
+			})
 
-		It("should export metrics", func() {
-			pods := &corev1.PodList{}
-			getResourceSafe("kube-system", "pods", "", "app.kubernetes.io/component=coil-egress-controller", pods)
-			Expect(pods.Items).Should(HaveLen(2))
+			It("should export metrics", func() {
+				pods := &corev1.PodList{}
+				getResourceSafe("kube-system", "pods", "", "app.kubernetes.io/component=coil-egress-controller", pods)
+				Expect(pods.Items).Should(HaveLen(2))
 
-			node := pods.Items[0].Spec.NodeName
+				node := pods.Items[0].Spec.NodeName
 
-			out, err := runOnNode(node, "curl", "-sf", fmt.Sprintf("http://%s:9396/metrics", pods.Items[0].Status.PodIP))
-			Expect(err).ShouldNot(HaveOccurred())
+				out, err := runOnNode(node, "curl", "-sf", fmt.Sprintf("http://%s:9396/metrics", pods.Items[0].Status.PodIP))
+				Expect(err).ShouldNot(HaveOccurred())
 
-			mfs, err := (&expfmt.TextParser{}).TextToMetricFamilies(bytes.NewReader(out))
-			Expect(err).NotTo(HaveOccurred())
-			Expect(mfs).NotTo(BeEmpty())
+				mfs, err := (&expfmt.TextParser{}).TextToMetricFamilies(bytes.NewReader(out))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(mfs).NotTo(BeEmpty())
+			})
 		})
 	}
 })
